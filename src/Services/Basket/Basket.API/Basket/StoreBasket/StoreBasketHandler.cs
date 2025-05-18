921906706aa206bf5d6f621c6c9522e35dae2a4f
AhsanAkhtarINF <ahsan.akhtar@infosalons.ae>
2025-05-19 00:01:30 +0200

    Till Section#8

---
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -0,0 +1,32 @@
+﻿
+
+
+
+namespace Basket.API.Basket.StoreBasket;
+
+public record StoreBasketComand(ShoppingCart Cart): ICommand<StoreBasketResult>;
+
+public record StoreBasketResult(string UserName);
+
+public class StoreBasketCommandValidator: AbstractValidator<StoreBasketComand>
+{
+    public StoreBasketCommandValidator()
+    {
+        RuleFor(x => x.Cart).NotNull().WithMessage("Cart can not be null");
+        RuleFor(x => x.Cart.UserName).NotEmpty().WithMessage("UserName is required");
+    }
+}
+public class StoreBasketCommandHandler(IBasketRepository repository) :
+    ICommandHandler<StoreBasketComand, StoreBasketResult>
+{
+    public async Task<StoreBasketResult> Handle(StoreBasketComand command, CancellationToken cancellationToken)
+    {
+        ShoppingCart cart = command.Cart;
+
+        //TODO: store basket in db
+        //TODO:  update cache
+
+        await repository.StoreBasket(command.Cart,cancellationToken);
+        return new StoreBasketResult(command.Cart.UserName);
+    }
+}
---
+++ src/Services/Basket/Basket.API/GlobalUsing.cs
@@ -0,0 +1,12 @@
+﻿global using Basket.API.Models;
+global using BuildingBlocks.CQRS;
+global using Carter;
+global using MediatR;
+global using Mapster;
+global using BuildingBlocks.Behaviors;
+global using FluentValidation;
+global using System.Diagnostics.Eventing.Reader;
+global using BuildingBlocks.Exceptions;
+global using Basket.API.Exception;
+global using Marten;
+global using Basket.API.Data;
