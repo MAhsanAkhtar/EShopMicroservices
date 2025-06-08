2bc0482fec0ea68310f484b0bcfc238fc2b3cd5f
AhsanAkhtarINF <ahsan.akhtar@infosalons.ae>
2025-06-08 07:26:39 +0200

    Section#17

--- src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -2,6 +2,9 @@
 
 
 
+using Discount.Grpc;
+using Microsoft.VisualBasic;
+
 namespace Basket.API.Basket.StoreBasket;
 
 public record StoreBasketComand(ShoppingCart Cart): ICommand<StoreBasketResult>;
@@ -16,17 +19,27 @@ public class StoreBasketCommandValidator: AbstractValidator<StoreBasketComand>
         RuleFor(x => x.Cart.UserName).NotEmpty().WithMessage("UserName is required");
     }
 }
-public class StoreBasketCommandHandler(IBasketRepository repository) :
+public class StoreBasketCommandHandler
+    (IBasketRepository repository, DiscountProtoService.DiscountProtoServiceClient discountProto) :
     ICommandHandler<StoreBasketComand, StoreBasketResult>
 {
     public async Task<StoreBasketResult> Handle(StoreBasketComand command, CancellationToken cancellationToken)
     {
         ShoppingCart cart = command.Cart;
 
-        //TODO: store basket in db
-        //TODO:  update cache
-
-        await repository.StoreBasket(command.Cart,cancellationToken);
+        //TODO: Communicate with Discount.Grpc and Caculate the latest prices of product with their discount
+        await DeductDiscount(discountProto, command, cancellationToken);
+        // Store basket in the database (use Marten upsert - if exist = update, if not exist insert)
+        await repository.StoreBasket(command.Cart, cancellationToken);
         return new StoreBasketResult(command.Cart.UserName);
+    }
+
+    private async Task DeductDiscount(DiscountProtoService.DiscountProtoServiceClient discountProto, StoreBasketComand command, CancellationToken cancellationToken)
+    {
+        foreach (var item in command.Cart.Items)
+        {
+            var coupon = await discountProto.GetDiscountAsync(new GetDiscountRequest { ProductName = item.ProductName }, cancellationToken: cancellationToken);
+            item.Price -= coupon.Amount;
+        }
     }
 }
